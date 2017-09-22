b904bcf552bf872cf14f62610bc3b6da098f9c1c
Trabalho <Trabalho@10.10.4.245>
2017-09-22 08:18:36 -0300

    Atualizando

--- src/br/com/luismatos/implementation/UsuarioDAOImplementation.java
+++ src/br/com/luismatos/implementation/UsuarioDAOImplementation.java
@@ -15,92 +15,58 @@ import br.com.luismatos.model.Usuario;
 
 public class UsuarioDAOImplementation extends GenericDAOImplementation<Usuario> implements UsuarioDAO {
 
-	Logger logger;
+    Logger logger;
 
-	public UsuarioDAOImplementation() {
-		super(Usuario.class);
-	}
+    public UsuarioDAOImplementation() {
+        super(Usuario.class);
+    }
 
-	public boolean existeEmail(Usuario usuario) {
+    public boolean existeEmail(Usuario usuario) {
 
-		if (!entityManager.getTransaction().isActive()) {
-			entityManager.getTransaction().begin();
-		}
+        if (!entityManager.getTransaction().isActive()) {
+            entityManager.getTransaction().begin();
+        }
 
-		Query query = entityManager.createQuery("select u from Usuario u where u.email = :pEmail ")
-				.setParameter("pEmail", usuario.getEmail());
+        Query query = entityManager.createQuery("select u from Usuario u where u.email = :pEmail ")
+                .setParameter("pEmail", usuario.getEmail());
 
-		boolean encontrado = !query.getResultList().isEmpty();
-		System.out.println("Existe usuario " + usuario.getEmail() + "? " + encontrado);
-		entityManager.flush();
-		entityManager.getTransaction().commit();
-		return encontrado;
+        boolean encontrado = !query.getResultList().isEmpty();
+        System.out.println("Existe usuario " + usuario.getEmail() + "? " + encontrado);
+        entityManager.flush();
+        entityManager.getTransaction().commit();
+        return encontrado;
 
-	}
+    }
 
-	/*
-	 * public Usuario existeUsuario(Usuario usuario){ Usuario usu = null;
-	 * 
-	 * if(!entityManager.getTransaction().isActive()){
-	 * entityManager.getTransaction().begin(); }
-	 * 
-	 * 
-	 * Query query = entityManager.
-	 * createQuery("select u from Usuario u where u.email = :pEmail and u.senha = :pSenha"
-	 * ).setParameter("pEmail", usuario.getEmail()).setParameter("pSenha",
-	 * usuario.getSenha()); boolean encontrado =
-	 * !query.getResultList().isEmpty();
-	 * 
-	 * if(encontrado == true){ usu = (Usuario) entityManager.
-	 * createQuery("select u from Usuario u where u.email = :pEmail and u.senha = :pSenha"
-	 * ).setParameter("pEmail", usuario.getEmail()).setParameter("pSenha",
-	 * usuario.getSenha()).getSingleResult(); }
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * //System.out.println("Existe Usuário " + usuario.getEmail() + "? " +
-	 * encontrado); entityManager.flush();
-	 * entityManager.getTransaction().commit(); return usu; }
-	 */
-	public Usuario existeUsuario(Usuario usuario) {
+    public Usuario existeUsuario(Usuario usuario) {
+        try {
+            if (!entityManager.getTransaction().isActive()) {
+                entityManager.getTransaction().begin();
+            }
+            CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+            CriteriaQuery<Usuario> criteria = builder.createQuery(Usuario.class);
 
-		try {
-			if (!entityManager.getTransaction().isActive()) {
-				entityManager.getTransaction().begin();
-			}
-			
-			CriteriaBuilder builder = entityManager.getCriteriaBuilder();
-			CriteriaQuery<Usuario> criteria = builder.createQuery(Usuario.class);
+            Root<Usuario> usuarioRoot = criteria.from(Usuario.class);
+            Predicate predicate = builder.and();
 
-			Root<Usuario> usuarioRoot = criteria.from(Usuario.class);
-			Predicate predicate = builder.and();
-			
-			predicate = builder.and(predicate, builder.equal(usuarioRoot.get("email"), usuario.getEmail()));
-			predicate = builder.and(predicate, builder.equal(usuarioRoot.get("senha"), usuario.getSenha()));
-			
-			TypedQuery<Usuario> typedQuery = entityManager.createQuery(criteria.select(usuarioRoot).where(predicate));
-			
-			Usuario usu = null;
-			if(!typedQuery.getResultList().isEmpty()){
-				usu = typedQuery.getSingleResult();
-				System.out.println(usu.toString());
-			}
-			
-			
-			//usu = entityManager.createQuery(criteria).getSingleResult();
-			
-			return usu;
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}finally {
-			entityManager.flush();
-			entityManager.getTransaction().commit();
-		}
-	}
+            predicate = builder.and(predicate, builder.equal(usuarioRoot.get("email"), usuario.getEmail()));
+            predicate = builder.and(predicate, builder.equal(usuarioRoot.get("senha"), usuario.getSenha()));
+
+            TypedQuery<Usuario> typedQuery = entityManager.createQuery(criteria.select(usuarioRoot).where(predicate));
+
+            Usuario usu = null;
+            if (!typedQuery.getResultList().isEmpty()) {
+                usu = typedQuery.getSingleResult();
+                System.out.println(usu.toString());
+            }
+            return usu;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            entityManager.flush();
+            entityManager.getTransaction().commit();
+        }
+    }
 
 }
